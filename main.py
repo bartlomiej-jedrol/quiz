eddcfe1161be03a4cbfc98fe5b09a7aff06f973d
bartlomiej-jedrol <bartlomiej.jedrol@gmail.com>
2024-02-05 15:49:21 +0100

    modified question bank

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ from question import Question
 from data import question_data
 from quiz_brain import QuizBrain
 
+
 question_bank = []
 for question in question_data:
-    question_bank.append(Question(text=question["text"], answer=question["answer"]))
+    question_bank.append(
+        Question(text=question["question"], answer=question["correct_answer"].lower())
+    )
 
 quiz = QuizBrain(question_list=question_bank)
 
